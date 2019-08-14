1b96ee7b673da71eb9082db99797ff5eb032ec1b
Chinedu <eduurbanus42@gmail.com>
2019-08-14 18:45:13 -0500

    added the layout customized

--- Project Code/src/main/java/edu/mum/cs/salmans/controllers/HomeController.java
+++ Project Code/src/main/java/edu/mum/cs/salmans/controllers/HomeController.java
@@ -12,10 +12,18 @@ import java.time.LocalDate;
 @Controller
 public class HomeController {
 
-//    @RequestMapping(value= PageUrlLocator.HOME_URL)
-//    public String displayHome(Model model) {
-//        model.addAttribute("page_links", AppHelper.publicPageLinks);
-//        model.addAttribute("now", LocalDate.now());
-//        return PageFileLocator.HOME_PAGE.toString();
-//    }
+    @RequestMapping(value= PageUrlLocator.HOME_URL)
+    public String displayHome(Model model) {
+        model.addAttribute("page_links", AppHelper.publicPageLinks);
+        model.addAttribute("now", LocalDate.now());
+        return PageFileLocator.HOME_PAGE.toString();
+    }
+
+    @RequestMapping(value= PageUrlLocator.LOGIN_URL)
+    public String displayLogin(Model model) {
+        model.addAttribute("page_links", AppHelper.publicPageLinks);
+        model.addAttribute("now", LocalDate.now());
+        return PageFileLocator.LOGIN_PAGE.toString();
+    }
+
 }
