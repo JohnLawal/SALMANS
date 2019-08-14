44bc2a363b2da241356b3cdb8e35f816f9ee8e3c
JohnLawal <jilawal@mum.edu>
2019-08-14 11:49:30 -0500

    Added customers dashboard

--- Project Code/src/main/java/edu/mum/cs/salmans/SalmansApplication.java
+++ Project Code/src/main/java/edu/mum/cs/salmans/SalmansApplication.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SalmansApplication implements CommandLineRunner {
     private AppointmentServiceImplementation appointmentServiceImplementation;
     private UserServiceImplementation userServiceImplementation;
--- Project Code/src/main/java/edu/mum/cs/salmans/controllers/UserController.java
+++ Project Code/src/main/java/edu/mum/cs/salmans/controllers/UserController.java
@@ -1,7 +1,39 @@
 package edu.mum.cs.salmans.controllers;
 
+import edu.mum.cs.salmans.serviceImpl.AppointmentServiceImplementation;
+import edu.mum.cs.salmans.utility.AppHelper;
+import edu.mum.cs.salmans.utility.AppValues;
+import edu.mum.cs.salmans.utility.PageFileLocator;
+import edu.mum.cs.salmans.utility.PageUrlLocator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
+    @Autowired
+    AppointmentServiceImplementation appointmentServiceImplementation;
+
+    @GetMapping(value = {PageUrlLocator.CUSTOMER_DASHBOARD_URL})
+    public ModelAndView displayCustomerDashboardPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("pageTitle", "Welcome To Our Appointment Scheduling App!");
+        modelAndView.addObject("pageLinks", AppHelper.customerPageLinks);
+        modelAndView.setViewName(PageFileLocator.CUSTOMER_DASHBOARD_PAGE.toString());
+        return modelAndView;
+    }
+
+
+    @GetMapping(value = { PageUrlLocator.CUSTOMER_VIEW_APPOINTMENTS_URL})
+    public ModelAndView listStudents(@RequestParam(defaultValue = "0") int page) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("pageTitle", "List of Your Appointments");
+        modelAndView.addObject("pageLinks", AppHelper.customerPageLinks);
+//        modelAndView.addObject("appointments", appointmentServiceImplementation.get);
+        modelAndView.addObject("currentPageNo", page);
+        modelAndView.setViewName(PageFileLocator.CUSTOMER_VIEW_APPOINTMENTS_PAGE.toString());
+        return modelAndView;
+    }
 }
